36ad49b43028e62d2047d7cbfcd4ca086a709d2a
Chester Wang <chesteraigo@dhcp-206-12-43-98.ubcsecure.wireless.ubc.ca>
2023-02-15 13:14:34 -0800

    Added a merged dataset and script to download all the data

---
+++ data/download_raw_data.sh
@@ -0,0 +1,17 @@
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/miscellaneous_divers/nutrient_value_miscellaneous_foods_2008_valeur_nutritive_divers-eng.csv > 1.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/fish_poissons/nutrient_value_fish_and_shellfish_2008_valeur_nutritive_poissons_et_crustaces-eng.csv>2.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/fats-oils_matieres-huiles/nutrient_value_fats_and_oils_foods_2008_valeur_nutritive_matieres_grasses_et_huiles-eng.csv > 3.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/breads-pains/nutrient_value_breads_cereals_and_other_grain_products_2008_valeur_nutritive_pains_cereales_et_autres_produits_cerealiers-eng.csv > 4.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/eggs_oeufs/nutrient_value_eggs_and_egg_dishes_2008_valeur_nutritive_oeufs_et_plats_base_oeufs-eng.csv > 5.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/fruit-fruits/nutrient_value_fruits_and_fruit_juices_2008_valeur_nutritive_fruits_et_jus_de_fruits-eng.csv > 6.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/vegetables-legumes/nutrient_value_vegetables_and_vegetable_products_2008_valeur_nutritive_legumes_et_produits_base_de_legumes-eng.csv > 7.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/soups-soupes/nutrient_value_some_common_foods_soups-2008-valeur_nutritive_quelques_aliments_usuels_soupe-eng.csv > 8.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/dairy-foods_produits-laitiers/nutrient_value_dairy_foods_and_other_related_products_2008_valeur_nutritive_produits_laitiers_et_produits_annexes-eng.csv > 9.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/baked-goods_boulangerie/nutrient_value_baked_2008_valeur_nutritive_boulangerie-eng.csv > 10.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/fast-foods_restaurant-minute/nutrient_value_fast_foods_2008_valeur_nutritive_restaurant_minute-eng.csv > 11.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/meat_viande/nutrient_value_meat_and_poultry_2008_valeur_nutritive_2008_viande_et_volaille-eng.csv > 12.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/legumes-nuts_legumineuses-noix/nutrient_value_legumes_nuts_and_seeds_2008_valeur_nutritive_legumineuses_noix_et_graines-eng.csv > 13.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/sugars-sucres/nutrient_value_some_common_foods_sweets_sugars-2008-valeur_nutritive_quelques_aliments_usuels_sucres_sucreries-eng.csv > 14.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/mixed-dishes_mets-composes/nutrient_value_mixed_dishes_2008_valeur_nutritive_mets_composes-eng.csv > 15.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/snacks_grignotises/nutrient_value_snacks_2008_valeur_nutritive_grignotises-eng.csv > 16.csv
+curl https://www.hc-sc.gc.ca/data-donnees/hpfb-dgpsa/beverages_boissons/nutrient_value_beverages_2008_valeur_nutritive_boissons-eng.csv > 17.csv
