9694a3cebc89370e34e5798249fe98db3a146395
Austin Shih <austinsh@student.ubc.ca>
2023-03-17 14:37:42 -0700

    update theme, increase readability/ useability

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(scales)
 library(RColorBrewer)
 library(plotly)
+library(shinythemes)
 
 server <- function(input, output, session) {
 
@@ -332,226 +333,211 @@ observeEvent(input$selectText,{
 
 
 ui <- navbarPage(
+  theme = shinytheme("spacelab"),
   'MacroView',
   # App title goes here!
   # dashboard tab
   tabPanel(
     'Dashboard',
     # first navbar page
-    headerPanel("Macroview Main Dashboard"),
-    fluidPage(fluidRow(
-      #2,
-
-      # Leftmost Input Panel
-      column(
-        #1
-
-        titlePanel("Enter Nutrient Targets"),
-        actionButton("selectSliders", "Plot Sliders", class = "btn-block"),
-        numericInput(
-          "calSliders",
-          "Enter Calorie Goal",
-          value = 2000,
-          min = 0,
-          max = 20000
-        ),
-        sliderInput(
-          "proteinSlider",
-          "Protein %",
-          value = 25,
-          min = 0,
-          max = 100
-        ),
-        sliderInput(
-          "carbSlider",
-          "Carb %",
-          value = 50,
-          min = 0,
-          max = 100
-        ),
-        sliderInput(
-          "fatSlider",
-          "Fat %",
-          value = 25,
-          min = 0,
-          max = 100
-        ),
-        tableOutput("table_sliders"),
-        actionButton("selectText", "Plot Manual Input?", class = "btn-block"),
-        textInput("proteinText", "Protein (Grams)",
-                  value = 100),
-        textInput("carbText", "Carbs (Grams)",
-                  value = 100),
-        textInput("fatText", "Fat (Grams)",
-                  value = 100),
-        tableOutput("table_text"),
-        width = 3,
-        offset = 0
-
+    h1('Macroview Main Dashboard'),
+    sidebarLayout(
+      # sidebar panel for inputs
+      sidebarPanel(
+        width=5,
+        fluidRow(
+          # target input 
+          column(
+            width=5,
+            h4("Enter Nutrient Targets"),
+            actionButton("selectSliders", "Plot Sliders", class = "btn-block"),
+            numericInput(
+              "calSliders",
+              "Enter Calorie Goal",
+              value = 2000,
+              min = 0,
+              max = 20000
+            ),
+            sliderInput(
+              "proteinSlider",
+              "Protein %",
+              value = 25,
+              min = 0,
+              max = 100
+            ),
+            sliderInput(
+              "carbSlider",
+              "Carb %",
+              value = 50,
+              min = 0,
+              max = 100
+            ),
+            sliderInput(
+              "fatSlider",
+              "Fat %",
+              value = 25,
+              min = 0,
+              max = 100
+            ),
+            tableOutput("table_sliders"),
+            actionButton("selectText", "Plot Manual Input", class = "btn-block"),
+            textInput("proteinText", "Protein (Grams)",
+                      value = 100),
+            textInput("carbText", "Carbs (Grams)",
+                      value = 100),
+            textInput("fatText", "Fat (Grams)",
+                      value = 100),
+            tableOutput("table_text")
+          ),
+          
+          # user input entry
+          column(
+            width=7,
+            h4("Food Entry"),
+            fluidRow(
+              column(
+                h5('Foods'),
+                selectInput(
+                  inputId = 'select_food1',
+                  label = 'Select food 1',
+                  choices = 'Names',
+                  selected = 'None',
+                  width = '100%'
+                ),
+                width = 8,
+                offset = 0
+              ),
+              column(
+                h5('Weights'),
+                numericInput(
+                  "g1",
+                  "Grams",
+                  value = 0,
+                  min = 0,
+                  max = 9000
+                ),
+                width = 4,
+                offset = 0
+              )
+            ),
+  
+            # mid panel row 2
+            fluidRow(
+              column(
+                selectInput(
+                  inputId = 'select_food2',
+                  label = 'Select food 2',
+                  choices = 'Names',
+                  selected = 'None',
+                  width = '100%'
+                ),
+                width = 8,
+                offset = 0
+              ),
+              column(
+                numericInput(
+                  "g2",
+                  "Grams",
+                  value = 0,
+                  min = 0,
+                  max = 9000
+                ),
+                width = 4,
+                offset = 0
+              )
+            ),
+  
+            # mid panel row 3
+            fluidRow(
+              column(
+                selectInput(
+                  inputId = 'select_food3',
+                  label = 'Select food 3',
+                  choices = 'Names',
+                  selected = 'None',
+                  width = '100%'
+                ),
+                width = 8,
+                offset = 0
+              ),
+              column(
+                numericInput(
+                  "g3",
+                  "Grams",
+                  value = 0,
+                  min = 0,
+                  max = 9000
+                ),
+                width = 4,
+                offset = 0
+              )
+            ),
+  
+            # mid panel row 4
+            fluidRow(
+              column(
+                selectInput(
+                  inputId = 'select_food4',
+                  label = 'Select food 4',
+                  choices = 'Names',
+                  selected = 'None',
+                  width = '100%'
+                ),
+                width = 8,
+                offset = 0
+              ),
+              column(
+                numericInput(
+                  "g4",
+                  "Grams",
+                  value = 0,
+                  min = 0,
+                  max = 9000
+                ),
+                width = 4,
+                offset = 0
+              )
+            ),
+  
+            # mid panel row 5
+            fluidRow(
+              column(
+                selectInput(
+                  inputId = 'select_food5',
+                  label = 'Select food 5',
+                  choices = 'Names',
+                  selected = 'None',
+                  width = '100%'
+                ),
+                width = 8,
+                offset = 0
+              ),
+              column(
+                numericInput(
+                  "g5",
+                  "Grams",
+                  value = 0,
+                  min = 0,
+                  max = 9000
+                ),
+                width = 4,
+                offset = 0
+              )
+            )
+          )
+        )
       ),
-      # END leftmost panel
-
-      # Middle Panel (Drop Downs)
-      column(
-        #2
-        titlePanel("Food Entry"),
-
-        # mid panel row 1
-        fluidRow(
-          column(
-            h3('Foods'),
-            #titlePanel("Food Entry"),
-            selectInput(
-              inputId = 'select_food1',
-              label = 'Select food 1',
-              choices = 'Names',
-              selected = 'None',
-              width = '100%'
-            ),
-            width = 9,
-            offset = 0
-          ),
-          column(
-            h3('Weights'),
-            #titlePanel("Weight"),
-            numericInput(
-              "g1",
-              "Grams",
-              value = 0,
-              min = 0,
-              max = 9000
-            ),
-            width = 3,
-            offset = 0
-          )
-        ),
-
-        # mid panel row 2
-        fluidRow(
-          column(
-            selectInput(
-              inputId = 'select_food2',
-              label = 'Select food 2',
-              choices = 'Names',
-              selected = 'None',
-              width = '100%'
-            ),
-            width = 9,
-            offset = 0
-          ),
-          column(
-            numericInput(
-              "g2",
-              "Grams",
-              value = 0,
-              min = 0,
-              max = 9000
-            ),
-            width = 3,
-            offset = 0
-          )
-        ),
-
-        # mid panel row 3
-        fluidRow(
-          column(
-            selectInput(
-              inputId = 'select_food3',
-              label = 'Select food 3',
-              choices = 'Names',
-              selected = 'None',
-              width = '100%'
-            ),
-            width = 9,
-            offset = 0
-          ),
-          column(
-            numericInput(
-              "g3",
-              "Grams",
-              value = 0,
-              min = 0,
-              max = 9000
-            ),
-            width = 3,
-            offset = 0
-          )
-        ),
-
-        # mid panel row 4
-        fluidRow(
-          column(
-            selectInput(
-              inputId = 'select_food4',
-              label = 'Select food 4',
-              choices = 'Names',
-              selected = 'None',
-              width = '100%'
-            ),
-            width = 9,
-            offset = 0
-          ),
-          column(
-            numericInput(
-              "g4",
-              "Grams",
-              value = 0,
-              min = 0,
-              max = 9000
-            ),
-            width = 3,
-            offset = 0
-          )
-        ),
-
-        # mid panel row 5
-        fluidRow(
-          column(
-            selectInput(
-              inputId = 'select_food5',
-              label = 'Select food 5',
-              choices = 'Names',
-              selected = 'None',
-              width = '100%'
-            ),
-            width = 9,
-            offset = 0
-          ),
-          column(
-            numericInput(
-              "g5",
-              "Grams",
-              value = 0,
-              min = 0,
-              max = 9000
-            ),
-            width = 3,
-            offset = 0
-          )
-        ),
-
-        width = 3,
-        offset = 0
+      
+      # mainpanel for plots
+      mainPanel(
+        width=7,
+        h1("Macroutrient Totals"),
+        plotOutput("main_plot", width = "1000px"),
+        h1("Macronutrient Proportions"),
+        plotOutput("sub_plot", width = "1000px")
       ),
-      # End Middle Panel
-
-
-
-      # Right Panel (Visualizations)
-      column(
-        5,
-        titlePanel("Macroutrient Totals"),
-        plotOutput("main_plot", width = "600px"),
-        titlePanel("Macronutrient Proportions"),
-        plotOutput("sub_plot", width = "600px")
-      )
-      # End Right Panel
-
-
-
-
-
-    ))
+      position = "left"
+    )
   ),
 
   tabPanel('About', 'A Page to display some other static information'),
