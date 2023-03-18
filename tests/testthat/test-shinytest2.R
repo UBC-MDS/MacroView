4364a6b3a1198522652aa4a638285186e36be5b2
Jakob Thoms <j99thoms@gmail.com>
2023-03-18 10:49:19 -0700

    add test for 'plot manual input' button

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -6,3 +6,11 @@ test_that("{shinytest2} recording: main-dash_plot-sliders-button", {
   app$click("selectSliders")
   app$expect_values()
 })
+
+
+test_that("{shinytest2} recording: main-dash_plot-manual-button", {
+  app <- AppDriver$new(name = "main-dash_plot-manual-button", seed = 123, height = 656, 
+      width = 1235)
+  app$click("selectText")
+  app$expect_values()
+})
