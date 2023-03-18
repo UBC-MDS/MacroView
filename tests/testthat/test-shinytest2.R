2b4296ff20ccd3510deb2031d6a57a14eef4f951
Jakob Thoms <j99thoms@gmail.com>
2023-03-18 11:13:59 -0700

    test for manual goal entry

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -14,3 +14,14 @@ test_that("{shinytest2} recording: food5-entry", {
   app$set_inputs(g5 = 100)
   app$expect_values()
 })
+
+
+test_that("{shinytest2} recording: manual-input-boxes", {
+  app <- AppDriver$new(name = "manual-input-boxes", seed = 123, height = 656, width = 1235)
+  app$set_inputs(proteinText = "99")
+  app$set_inputs(carbText = "27")
+  app$set_inputs(fatText = "1")
+  app$set_inputs(fatText = "101")
+  app$set_inputs(fatText = "101.5")
+  app$expect_values()
+})
