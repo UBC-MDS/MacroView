b988ee8c0d4bb55334b1aa1ae206f4249f4edfa3
Jakob Thoms <j99thoms@gmail.com>
2023-03-18 10:48:22 -0700

    add test for 'plot sliders' button

---
+++ tests/testthat/test-shinytest2.R
@@ -0,0 +1,8 @@
+library(shinytest2)
+
+test_that("{shinytest2} recording: main-dash_plot-sliders-button", {
+  app <- AppDriver$new(name = "main-dash_plot-sliders-button", seed = 123, height = 656, 
+      width = 1235)
+  app$click("selectSliders")
+  app$expect_values()
+})
