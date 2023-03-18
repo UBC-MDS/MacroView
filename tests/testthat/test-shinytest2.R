bc1d9e553b93877d9cda6cc3bda9532291b0d547
Jakob Thoms <j99thoms@gmail.com>
2023-03-18 11:12:32 -0700

    test for 5th food entry

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -6,3 +6,11 @@ test_that("{shinytest2} recording: food4-entry", {
   app$set_inputs(g4 = 150)
   app$expect_values()
 })
+
+
+test_that("{shinytest2} recording: food5-entry", {
+  app <- AppDriver$new(name = "food5-entry", seed = 123, height = 656, width = 1235)
+  app$set_inputs(select_food5 = "Kiwifruit")
+  app$set_inputs(g5 = 100)
+  app$expect_values()
+})
